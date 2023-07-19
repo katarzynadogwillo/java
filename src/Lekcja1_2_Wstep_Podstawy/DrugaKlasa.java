7a6ab9d7f6e3633ced3fd25b608cfed835de4985
kasiadogwillo <katarzynadogwillo@gmail.com>
2023-07-19 11:50:47 +0200

    first lessons

---
+++ src/Lekcja1_2_Wstep_Podstawy/DrugaKlasa.java
@@ -0,0 +1,12 @@
+package Lekcja1_2_Wstep_Podstawy;
+
+public class DrugaKlasa {
+    public static void main(String[] args) {
+        System.out.println("Moja druga klasa");
+
+        int zmienna = 10;
+        int zmienna2 = 11;
+        System.out.println(zmienna + zmienna2);
+
+    }
+}
