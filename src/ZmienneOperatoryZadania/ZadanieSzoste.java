7c100dc975ed32836d11944d9b646a0861c85ccc
kasiadogwillo <katarzynadogwillo@gmail.com>
2023-07-24 11:20:36 +0200

    next lessons

---
+++ src/ZmienneOperatoryZadania/ZadanieSzoste.java
@@ -0,0 +1,14 @@
+package ZmienneOperatoryZadania;
+
+public class ZadanieSzoste {
+    public static void main(String[] args) {
+        //Przypisz wartość przekraczającą poza rozmiar zmiennej typu int.
+        //Zobacz co się stanie i zastanów dlaczego. Spróbuj oszukać środowisko, aby nie wytykało Ci błędów :)
+
+        int zmiennaInt = 1000000000;
+        int wiekszyInt = zmiennaInt + zmiennaInt + zmiennaInt + zmiennaInt + zmiennaInt;
+        int zaDuzyInt = wiekszyInt;
+
+        System.out.println("Za duży int to: " + zaDuzyInt);
+    }
+}
