0c2eb50874ed343732d6270ef1a496162e317a7b
kasiadogwillo <katarzynadogwillo@gmail.com>
2023-08-16 10:20:24 +0200

    next lessons

--- src/Lekcja12_Klasy/Klasa.java
+++ src/Lekcja12_Klasy/Klasa.java
@@ -1,24 +1,11 @@
 package Lekcja12_Klasy;
 
-public class Klasa { //class NazwaKlasy - cammel case = każdy wyraz nazywamy wielką literą
-    //ciało klasy
-    public static void main(String[] args) { //metoda
+public class Klasa {
 
-    }
-    //programowanie obiektowe - OOP - Object Oriented Programming
+    NowaKlasa nazwaDlaNowejKlasy = new NowaKlasa(); //został utworzony obiekt dla NowaKlasa, coś, co istnieje w pamięci
 
-    //w klasie przechowywane są metody, pola, stany itd.
 
-    //pola:
-    int jakiesPole;
-    boolean kolejnePole;
-    char nastepnePole;
 
-    //metody:
-    void jakasMetoda () { //jak dana klasa ma się zachować, zachowanie klasy
-    } // w tej chwili ta metoda nic nie wykonuje - nawias jest pusty, nie ma akcji
+}
 
-    void kolejnaMetoda () {
-    }
-
-} // tu się kończy klasa
+} //tu się kończy klasa
---
+++ src/Lekcja12_Klasy/NowaKlasa.java
@@ -0,0 +1,25 @@
+package Lekcja12_Klasy;
+
+public class NowaKlasa {
+
+    //pola - inaczej stany
+    int jakiesPole;
+    boolean kolejnePole;
+    char innePole;
+
+    //metody
+
+    void jakasMetoda () {
+
+    } //pusta metoda - zachowanie klasy, mówi jak klasa ma się zachować
+
+    void kolejnaMetoda () {
+
+    }
+
+    //ta klasa składa się z trzech pól i dwóch metod (nic nie wykonują, bo są puste);
+    // opisuje co posiada, jak się zachowuje (poprzez pola i metody)
+
+
+
+}
